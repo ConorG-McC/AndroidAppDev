2d9c7a349d32a24310868b8cda7ab3d1ef90ffc2
Conor Giffen-McCloskey <conor.giffen-mccloskey@bt.com>
2024-02-15 14:27:54 +0000

    Calculator tutorial tasks (incomplete)

--- app/src/main/java/com/example/androidappdev/MainActivity.kt
+++ app/src/main/java/com/example/androidappdev/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,11 +13,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -27,12 +28,13 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.androidappdev.ui.theme.AndroidAppDevTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
+            AndroidAppDevTheme {
                 OverallDisplay()
             }
         }
@@ -40,24 +42,49 @@ class MainActivity : ComponentActivity() {
 
     @Preview
     @Composable
-    fun OverallDisplay() {
+    fun OverallDisplay(modifier: Modifier = Modifier) {
         var total by remember { mutableIntStateOf(0) }
-        Display(textToDisplay = total.toString())
-        Spacer(modifier = Modifier.size(30.dp))
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .padding(50.dp)
-                .fillMaxWidth()
-        ) {
-            CustomButton(stringResource(R.string.add_button_text),
-                clickButton = { total++ })
-            CustomButton(stringResource(R.string.subtract_button_text),
-                clickButton = { total-- })
+        var output by remember { mutableStateOf("") }
+        var currentValue by remember { mutableStateOf("") }
+        Display(textToDisplay = output.toString())
+        Spacer(modifier = Modifier.size(300.dp))
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .padding(50.dp)
+                    .fillMaxWidth()
+            ) {
+                CustomButton(stringResource(R.string.one),
+                    clickButton = { currentValue += "1";
+                                    output = currentValue })
+                CustomButton(stringResource(R.string.four),
+                    clickButton = { currentValue += "4";
+                                    output = currentValue })
+                CustomButton(stringResource(R.string.seven),
+                    clickButton = { currentValue += "7" ;
+                                    output = currentValue})
+                CustomButton(stringResource(R.string.add),
+                    clickButton = { total += currentValue.toInt();
+                                    currentValue = String();
+                                    output = total.toString()})
+                CustomButton(stringResource(R.string.cancel),
+                    clickButton = { currentValue = ""; total = 0;
+                                    output = String() })
+                CustomButton(stringResource(R.string.equals),
+                    clickButton = { if(currentValue.isNotEmpty()) {
+                                        total += currentValue.toInt();
+                                        output = total.toString();
+                                        currentValue = String();
+                                        total=0
+                                    }
+                                    })
+
+
+            }
+
         }
     }
-
     @Composable
     fun CustomButton(text: String, clickButton: () -> Unit) {
         Button(
@@ -86,6 +113,6 @@ class MainActivity : ComponentActivity() {
             )
         }
     }
-}
+
 
 

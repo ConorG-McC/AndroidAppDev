95c984cb732a095ea84e79702c3ffb82b2321982
ConorG-McC <conorgiffenmccloskey@gmail.com>
2024-02-28 22:38:22 +0000

    Add dependencies for navigation and contact view functionality

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,11 @@ android {
 }
 
 dependencies {
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.compose.material:material:1.6.1")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.1")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")

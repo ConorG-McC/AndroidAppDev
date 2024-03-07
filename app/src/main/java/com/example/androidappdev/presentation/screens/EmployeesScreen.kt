1aa7d1ce00305852dddd964527bcc9c21eb9c754
Conor Giffen-McCloskey <conorgiffenmccloskey@gmail.com>
2024-03-07 14:39:37 +0000

    Add Employees icon and screen

---
+++ app/src/main/java/com/example/androidappdev/presentation/screens/EmployeesScreen.kt
@@ -0,0 +1,37 @@
+package com.example.androidappdev.presentation.screens
+
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import com.example.androidappdev.presentation.components.FloatingButton
+
+
+@Composable
+fun EmployeeScreen(text:String,
+               context: Context,
+               modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            text = text,
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+        )
+        FloatingButton("woop",
+            { Toast.makeText(context,"Woop", Toast.LENGTH_LONG).show()},
+            modifier)
+    }
+}

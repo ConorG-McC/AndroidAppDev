1817937221d1a48be146eeddfd70b388addad0d1
Conor Giffen-McCloskey <conorgiffenmccloskey@gmail.com>
2024-03-07 19:51:08 +0000

    Remove unused Contact data class

--- app/src/main/java/com/example/androidappdev/Contact.kt
+++
@@ -1,9 +0,0 @@
-package com.example.androidappdev
-
-data class Contact(
-    var firstName: String = String(),
-    var surname: String = String(),
-    var telNo: String = String()
-) {
-    override fun toString(): String = "$firstName $surname ($telNo)"
-}

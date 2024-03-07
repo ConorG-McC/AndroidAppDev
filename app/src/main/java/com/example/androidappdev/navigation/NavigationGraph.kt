9166bf75d7d92a2165668e73bcf968cfe9a20416
Conor Giffen-McCloskey <conorgiffenmccloskey@gmail.com>
2024-03-07 19:51:36 +0000

    Update Navigation graph with new screens

--- app/src/main/java/com/example/androidappdev/navigation/NavigationGraph.kt
+++ app/src/main/java/com/example/androidappdev/navigation/NavigationGraph.kt
@@ -8,11 +8,13 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.androidappdev.R
-import com.example.androidappdev.presentation.screens.EmployeeScreen
-import com.example.androidappdev.presentation.screens.TasksScreen
-import com.example.androidappdev.presentation.screens.HomeScreen
-import com.example.androidappdev.presentation.screens.LoginScreen
-import com.example.androidappdev.presentation.screens.TeamScreen
+import com.example.androidappdev.presentation.screens.employees.EmployeeScreen
+import com.example.androidappdev.presentation.screens.employees.add.AddEmployeeScreen
+import com.example.androidappdev.presentation.screens.tasks.TasksScreen
+import com.example.androidappdev.presentation.screens.home.HomeScreen
+import com.example.androidappdev.presentation.screens.login.LoginScreen
+import com.example.androidappdev.presentation.screens.tasks.add.AddTaskScreen
+import com.example.androidappdev.presentation.screens.team.TeamScreen
 import kotlin.system.exitProcess
 
 open class NavScreen(var icon:Int, var route:String){
@@ -20,7 +22,9 @@ open class NavScreen(var icon:Int, var route:String){
     object Home : NavScreen(R.drawable.home, "Home")
     object Team : NavScreen(R.drawable.team, "Team")
     object Employees : NavScreen(R.drawable.employees, "Employees")
+    object AddEmployee: NavScreen(R.drawable.employees, "AddEmployee")
     object Tasks: NavScreen(R.drawable.tasks, "Tasks")
+    object AddTask: NavScreen(R.drawable.tasks, "AddTask")
     object Exit: NavScreen(R.drawable.logout, "Logout")
 }
 
@@ -43,10 +47,17 @@ fun NavigationGraph(
             TeamScreen(stringResource(R.string.team_button),context, modifier)
         }
         composable(NavScreen.Employees.route) {
-            EmployeeScreen(stringResource(R.string.employee_button),context, modifier)
+            EmployeeScreen(stringResource(R.string.employee_button), modifier, navController)
+        }
+        composable(NavScreen.AddEmployee.route) {
+            AddEmployeeScreen(stringResource(R.string.add_employee_button), modifier, navController)
+
         }
         composable(NavScreen.Tasks.route) {
-            TasksScreen(stringResource(R.string.tasks_button), modifier)
+            TasksScreen(stringResource(R.string.tasks_button), modifier, navController)
+        }
+        composable(NavScreen.AddTask.route) {
+            AddTaskScreen(stringResource(R.string.add_task_button), modifier, navController)
         }
         composable(NavScreen.Exit.route) {
             exitProcess(0)

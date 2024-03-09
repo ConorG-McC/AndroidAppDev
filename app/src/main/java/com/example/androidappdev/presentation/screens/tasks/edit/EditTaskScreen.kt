8c46fd99d1d5ccbb8d00192d2ae60d2110407f78
Conor Giffen-McCloskey <conorgiffenmccloskey@gmail.com>
2024-03-09 22:41:59 +0000

    Add EditTaskScreen and EditTaskViewModel with functionality

---
+++ app/src/main/java/com/example/androidappdev/presentation/screens/tasks/edit/EditTaskScreen.kt
@@ -0,0 +1,59 @@
+package com.example.androidappdev.presentation.screens.tasks.edit
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.androidappdev.R
+import com.example.androidappdev.presentation.components.CustomButton
+import com.example.androidappdev.presentation.components.CustomTextField
+
+@Composable
+fun EditTaskScreen(vm: EditTaskViewModel = viewModel(factory = EditTaskViewModel.Factory),
+                   selectedTaskIndex: Int,
+                   onClickToHome: () -> Unit
+) {
+
+    vm.getTasks(selectedTaskIndex) //called each time component is updated
+
+    Column(modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            text = "Edit",
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+        )
+        Column {
+            CustomTextField(stringResource(R.string.title_hint),
+                            text = vm.title,
+                            onNameChange = { vm.title = it },
+                            stringResource(R.string.title_error_message),
+                            vm.titleIsValid()
+            )
+
+            CustomTextField(stringResource(R.string.description_hint),
+                            text = vm.description,
+                            onNameChange = { vm.description = it },
+                            stringResource(R.string.description_error_message),
+                            vm.descriptionIsValid()
+            )
+
+            CustomButton(stringResource(R.string.save), clickButton = {
+                vm.updateTask()
+                onClickToHome()
+            })
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/androidappdev/presentation/screens/tasks/edit/EditTaskViewModel.kt
@@ -0,0 +1,53 @@
+package com.example.androidappdev.presentation.screens.tasks.edit
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.androidappdev.core.MyAppApplication
+import com.example.androidappdev.data.`object`.Task
+import com.example.androidappdev.data.repositories.TaskInMemoryRepository
+
+class EditTaskViewModel(private val repo: TaskInMemoryRepository) :
+    ViewModel() {
+    private var selectedTask: Task? = null
+
+    var title by mutableStateOf("")
+    var description by mutableStateOf("")
+    fun titleIsValid(): Boolean {
+        return title.isNotBlank()
+    }
+
+    fun descriptionIsValid(): Boolean {
+        return description.isNotBlank()
+    }
+
+    fun getTasks(selectedIndex: Int) {//Display when screen loads
+        if (selectedTask == null) {
+            selectedTask = repo.getAllTasks()[selectedIndex]
+            Log.v("OK", selectedTask!!.toString())
+            title = selectedTask!!.title
+            description = selectedTask!!.description
+        }
+    }
+
+    fun updateTask() {
+        selectedTask!!.title = title
+        selectedTask!!.description = description
+        repo.edit(selectedTask!!)
+    }
+
+    // Define ViewModel factory in a companion object
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                EditTaskViewModel(repo = MyAppApplication.taskInMemoryRepository
+                )
+            }
+        }
+    }
+}

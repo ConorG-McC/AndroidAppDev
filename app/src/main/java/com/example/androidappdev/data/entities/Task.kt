86b2a26abde27bdc3a30fdc33b3e88cc3adf787b
Conor Giffen-McCloskey <conorgiffenmccloskey@gmail.com>
2024-03-13 00:14:07 +0000

    Add Task Status to Task object

--- app/src/main/java/com/example/androidappdev/data/entities/Task.kt
+++ app/src/main/java/com/example/androidappdev/data/entities/Task.kt
@@ -1,12 +1,13 @@
 package com.example.androidappdev.data.entities
 
+import com.example.androidappdev.data.enums.TaskStatus
 import java.util.UUID
 
-data class Task(
-        var id: UUID,
-        var title: String = String(),
-        var description: String = String(),
+data class Task(var id: UUID,
+                var title: String = String(),
+                var description: String = String(),
+                var taskStatus: TaskStatus = TaskStatus.TODO
 ) {
     override fun toString(): String =
-        " Task ID: $id \n Task Title: $title \n Task Description: $description"
+        " Task ID: $id \n Task Title: $title \n Task Description: $description \n Task Status: $taskStatus"
 }

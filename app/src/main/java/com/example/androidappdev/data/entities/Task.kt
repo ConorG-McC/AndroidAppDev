97ab2074d2f1259d4dab3acf807314967d31626f
Conor Giffen-McCloskey <conorgiffenmccloskey@gmail.com>
2024-03-14 17:11:43 +0000

    Add status property to Task object as well as refactor task id property

--- app/src/main/java/com/example/androidappdev/data/entities/Task.kt
+++ app/src/main/java/com/example/androidappdev/data/entities/Task.kt
@@ -1,12 +1,15 @@
 package com.example.androidappdev.data.entities
 
+import com.example.androidappdev.data.enums.TaskStatus
 import java.util.UUID
 
 data class Task(
-        var id: UUID,
         var title: String = String(),
         var description: String = String(),
+        var status: TaskStatus = TaskStatus.TODO
 ) {
+    var id: String? = null
+
     override fun toString(): String =
-        " Task ID: $id \n Task Title: $title \n Task Description: $description"
+        " Task ID: $id \n Task Title: $title \n Task Description: $description \n Task Status: $status"
 }

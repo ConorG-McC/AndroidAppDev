08778f8201f2e45197876d833b1e913f11fc4bd4
Conor Giffen-McCloskey <conorgiffenmccloskey@gmail.com>
2024-04-14 22:20:35 +0100

    Task functionality now adds task under current userid and will only show tasks added by that user

--- app/src/main/java/com/example/androidappdev/presentation/screens/tasks/edit/EditTaskViewModel.kt
+++ app/src/main/java/com/example/androidappdev/presentation/screens/tasks/edit/EditTaskViewModel.kt
@@ -8,25 +8,27 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.androidappdev.core.MyAppApplication
+import com.example.androidappdev.data.auth.AuthRepo
 import com.example.androidappdev.data.task.Task
 import com.example.androidappdev.data.task.TaskRepo
 
-class EditTaskViewModel(private val repo: TaskRepo) :
-    ViewModel() {
+class EditTaskViewModel(private val authRepo: AuthRepo,
+                        private val repo: TaskRepo
+) : ViewModel() {
     private var selectedTask: Task? = null
 
     var id by mutableStateOf("")
     var title by mutableStateOf("")
     var description by mutableStateOf("")
     var taskStatus by mutableStateOf("")
-
-    fun setSelectedTask(task: Task){
+    fun setSelectedTask(task: Task) {
         id = task.id.toString()
         title = task.title.toString()
         description = task.description.toString()
         taskStatus = task.status.toString()
         selectedTask = task
     }
+
     fun titleIsValid(): Boolean {
         return title.isNotBlank()
     }
@@ -38,14 +40,15 @@ class EditTaskViewModel(private val repo: TaskRepo) :
     fun updateTask() {
         selectedTask!!.title = title
         selectedTask!!.description = description
-        repo.editTask(selectedTask!!)
+        repo.editTask(selectedTask!!, authRepo.currentUser!!.uid)
     }
 
     // Define ViewModel factory in a companion object
     companion object {
         val Factory: ViewModelProvider.Factory = viewModelFactory {
             initializer {
-                EditTaskViewModel(repo = MyAppApplication.container.taskRepository
+                EditTaskViewModel(authRepo = MyAppApplication.container.authRepository,
+                                  repo = MyAppApplication.container.taskRepository
                 )
             }
         }
--- app/src/main/java/com/example/androidappdev/presentation/screens/tasks/view/TaskViewModel.kt
+++ app/src/main/java/com/example/androidappdev/presentation/screens/tasks/view/TaskViewModel.kt
@@ -7,39 +7,45 @@ import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.androidappdev.core.MyAppApplication
-import com.example.androidappdev.data.task.Task
-import com.example.androidappdev.data.task.TaskRepo
+import com.example.androidappdev.data.auth.AuthRepo
 import com.example.androidappdev.data.database.DatabaseResult
 import com.example.androidappdev.data.database.DatabaseState
+import com.example.androidappdev.data.task.Task
+import com.example.androidappdev.data.task.TaskRepo
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-class TaskViewModel(private val repo: TaskRepo) : ViewModel() {
+class TaskViewModel(private val authRepo: AuthRepo,
+                    private val repo: TaskRepo
+) : ViewModel() {
     private val _userState = MutableStateFlow(DatabaseState<Task>())
-    val userState: StateFlow<DatabaseState<Task>> = _userState.asStateFlow()//Monitored by component for recomposition on change
+    val userState: StateFlow<DatabaseState<Task>> =
+        _userState.asStateFlow()//Monitored by component for recomposition on change
 
     val items = mutableStateListOf<Task>()
-    var selectedTask: Task?= null
-    fun taskHasBeenSelected(): Boolean = selectedTask!=null
+    var selectedTask: Task? = null
+    fun taskHasBeenSelected(): Boolean = selectedTask != null
 
     init {
-        getTasks()
+        getListOfTasks(authRepo.currentUser!!.uid)
     }
 
-    private fun getTasks() = viewModelScope.launch {
-        repo.getAllTasks().collect { result ->
-            when(result) {
+    private fun getListOfTasks(currentUserID: String) = viewModelScope.launch {
+        repo.getAllTasks(currentUserID).collect { result ->
+            when (result) {
                 is DatabaseResult.Success -> {
                     _userState.update { it.copy(data = result.data) }
                 }
+
                 is DatabaseResult.Error -> {
                     _userState.update {
                         it.copy(errorMessage = result.exception.message!!)
                     }
                 }
+
                 is DatabaseResult.Loading -> {
                     _userState.update { it.copy(isLoading = true) }
                 }
@@ -47,7 +53,7 @@ class TaskViewModel(private val repo: TaskRepo) : ViewModel() {
         }
     }
 
-    fun deleteTask(){
+    fun deleteTask() {
         if (taskHasBeenSelected()) {
             repo.deleteTask(selectedTask!!)
             selectedTask = null
@@ -58,7 +64,8 @@ class TaskViewModel(private val repo: TaskRepo) : ViewModel() {
     companion object {
         val Factory: ViewModelProvider.Factory = viewModelFactory {
             initializer {
-                TaskViewModel(repo = MyAppApplication.container.taskRepository
+                TaskViewModel(authRepo = MyAppApplication.container.authRepository,
+                              repo = MyAppApplication.container.taskRepository
                 )
             }
         }
